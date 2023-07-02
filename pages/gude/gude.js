449c6c7a82159165e37ce591e2b23f4c1e31a0c6
liuwenhao <liuwenhao@live.com>
2023-07-02 16:14:06 +0800

    feat: develop gude front page

--- pages/gude/gude.js
+++ pages/gude/gude.js
@@ -16,6 +16,15 @@ Page({
     duration: 500,
     interval: 5000,
     swiperList,
+    img1:
+      "http://rwi9z0rl6.hd-bkt.clouddn.com/%E6%B4%97%E8%BD%A6%E5%B0%8F%E7%A8%8B%E5%BA%8F/%E5%8D%A1%E6%B3%A8%E5%86%8C.png",
+    img2:
+      "http://rwi9z0rl6.hd-bkt.clouddn.com/%E6%B4%97%E8%BD%A6%E5%B0%8F%E7%A8%8B%E5%BA%8F/%E9%92%B1%E5%8C%85.png",
+    img3:
+      "http://rwi9z0rl6.hd-bkt.clouddn.com/%E6%B4%97%E8%BD%A6%E5%B0%8F%E7%A8%8B%E5%BA%8F/%E9%A1%B9%E7%9B%AE.png",
+    border: {
+      color: "#f6f6f6",
+    },
   },
 
   onChange(e) {
