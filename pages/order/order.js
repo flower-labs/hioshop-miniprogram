2e6a5b6f5e71c5a7463860b14ef2814a8ca3ece7
julianlliu <julianlliu@tencent.com>
2023-08-26 21:12:26 +0800

    feat: 预约页面对接后端已预约数量和最近可预约时间

--- pages/order/order.js
+++ pages/order/order.js
@@ -8,17 +8,17 @@ Page({
      * 页面的初始数据
      */
     data: {
-        reserveList:[]
+        reserveList: [],
     },
-    get_order(e){
-        let index=e.currentTarget.dataset.id;
-        let service_price=this.data.reserveList[index].service_price;
-        let service_name=this.data.reserveList[index].service_name;
-        let id=this.data.reserveList[index].id;
+    get_order(e) {
+        let index = e.currentTarget.dataset.id;
+        let service_price = this.data.reserveList[index].service_price;
+        let service_name = this.data.reserveList[index].service_name;
+        let id = this.data.reserveList[index].id;
 
         // 跳转页面
         wx.navigateTo({
-          url: '/pages/orderDetail/orderDetail?service_id='+id+'&service_name='+service_name+'&service_price='+service_price,
+            url: '/pages/orderDetail/orderDetail?service_id=' + id + '&service_name=' + service_name + '&service_price=' + service_price,
         })
     },
 
@@ -27,7 +27,7 @@ Page({
      */
     onLoad(options) {
 
-       },
+    },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
@@ -41,13 +41,30 @@ Page({
     onShow() {
         this.getGoodsInfo()
     },
+    convertTime(timestamp) {
+        const date = new Date(timestamp * 1000);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        const seconds = String(date.getSeconds()).padStart(2, '0');
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    },
     //获取商品信息
-    getGoodsInfo(){
+    getGoodsInfo() {
         let that = this;
         util.request(api.ReserveList).then(function (res) {
-            console.log(res);
+            // 已预约数据
+            const dynamicList = res.data.reserveDynamicList;
+            // 返回数据中计算count字段
+            const reserveList = res.data.reserveList.map(item => ({
+                ...item,
+                count: dynamicList.find(countItem => String(item.id) === countItem.id).count,
+                latestTimestamp: that.convertTime(dynamicList.find(countItem => String(item.id) === countItem.id).latestTimestamp)
+            }))
             that.setData({
-                reserveList: res.data.reserveList,
+                reserveList: reserveList,
             });
         });
     },
