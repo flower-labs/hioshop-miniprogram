d1d4d11a21d7477160dc40bcecd068331c80d231
liuwenhao <liuwenhao@live.com>
2025-07-23 00:31:42 +0800

    feat: 更新获取默认baby逻辑

--- pages/baby-setting/utils.ts
+++ pages/baby-setting/utils.ts
@@ -5,11 +5,13 @@ export const queryBabyDetail = async () => {
   const resp = await util.request(api.GetBabyDetail, 'POST');
   if (resp.errno === 0) {
     if (resp.data.length !== 0) {
-      wx.setStorageSync('defaultBabyId', resp.data[0]?.id);
+      const sortBabyList = (resp.data || []).toSorted((a,b)=> a.create_time - b.create_time);
+      const defaultBaby = sortBabyList?.[0];
+      wx.setStorageSync('defaultBabyId', defaultBaby?.id);
       return {
         hasInfo: true,
         // TOOD: 更新取默认baby信息逻辑
-        babyInfo: resp.data[0],
+        babyInfo: defaultBaby,
       };
     } else {
       return {
